d8f27d8b6abe95a65b36d49ef82f50c69fa1e0d5
Ahmed <124316347+mahmedalam@users.noreply.github.com>
2023-09-01 17:49:38 +0500

    Add files via upload

---
+++ Image Converter.py
@@ -0,0 +1,63 @@
+import sys
+from ui import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PIL import Image
+import pillow_avif
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.window = QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.window)
+        self.ui.exportBtn.setEnabled(False)
+        self.window.show()
+
+        self.ui.openBtn.clicked.connect(lambda: self.openFile())
+        self.ui.exportBtn.clicked.connect(lambda: self.saveFile())
+
+
+    def openFile(self):
+        self.openFilePath = QFileDialog.getOpenFileName(self, "Open File", "", "All Supported Images(*.png *.jpg *.jpeg *.webp *.avif)")[0]
+        if self.openFilePath:
+            self.ui.exportBtn.setEnabled(True)
+
+    def saveFile(self):
+        self.saveFilePath = QFileDialog.getSaveFileName(self, "Save File", "", "All Supported Images(*.png *.jpeg *.webp)")[0]
+        if self.saveFilePath:
+            self.converter()
+
+
+    def converter(self):
+        # PNG
+        if self.ui.typeComboBox.currentText() == "PNG":
+            img = Image.open(self.openFilePath)
+            if self.saveFilePath.endswith(".png"):
+                img.save(self.saveFilePath, format="PNG")
+            else:
+                img.save(f"{self.saveFilePath}.png", format="PNG")
+
+        # JPEG
+        if self.ui.typeComboBox.currentText() == "JPEG":
+            img = Image.open(self.openFilePath)
+            img = img.convert("RGB")
+            if self.saveFilePath.endswith(".jpeg"):
+                img.save(self.saveFilePath, format="JPEG", quality=100, optimize=True)
+            else:
+                img.save(f"{self.saveFilePath[:-4]}.jpeg", format="JPEG", quality=100, optimize=True)
+
+        # WEBP
+        if self.ui.typeComboBox.currentText() == "WEBP":
+            img = Image.open(self.openFilePath)
+            if self.saveFilePath.endswith(".webp"):
+                img.save(self.saveFilePath, format="WEBP")
+            else:
+                img.save(f"{self.saveFilePath}.webp", format="WEBP")
+
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+sys.exit(app.exec_())
